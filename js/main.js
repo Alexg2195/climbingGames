0db606828295b805658f5bed1cd76b6867224475
Alexg2195 <alexg2195@gmail.com>
2016-02-06 02:40:39 -0500

    added resize function to timer with plus and minus keys when hovered over

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-  $(function() {
-    $( ".draggable" ).draggable();
+$(document).ready(function () {
+  var element = null;
+
+  $('#myStopWatch').on('mouseover', function(e) {
+      element = $(e.currentTarget);
   });
+
+  $('#myStopWatch').on('mouseout', function(e) {
+      element = null;
+  });
+
+  $(document).keypress(function(e) {
+      if (e.which == 61 && element) {
+        var newPxSize = parseInt($(".time").css("font-size"));
+        if (30 > newPxSize < 100) {
+          newPxSize += 10;
+          console.log(newPxSize);
+          newPxSize = newPxSize + "" + "px";
+          console.log(newPxSize);
+          $(".time").css("font-size", newPxSize);
+        };
+      }
+
+      if (e.which == 45 && element) {
+        var newPxSize = parseInt($(".time").css("font-size"));
+        if (30 > newPxSize < 100) {
+          newPxSize -= 10;
+          console.log(newPxSize);
+          newPxSize = newPxSize + "" + "px";
+          console.log(newPxSize);
+          $(".time").css("font-size", newPxSize);
+        };
+      }
+  });
+
+  $(".draggable").draggable();
+})
