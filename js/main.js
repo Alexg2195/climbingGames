ba5e0ecd65ef9919b42c67e1af192d88216229fc
Alexg2195 <alexg2195@gmail.com>
2016-02-06 04:31:30 -0500

    commented main.js and finished resizing functionality to icons

--- js/main.js
+++ js/main.js
@@ -1,38 +1,72 @@
 $(document).ready(function () {
   var circleCount = 0;
   var timerElement = null;
+  var iconElement = null;
+
+// Logging mouse over data for the timer and icons as a variable
 
   $('#myStopWatch').on('mouseover', function(e) {
-      timerElement = $(e.currentTarget);
+    timerElement = $(e.currentTarget);
   });
 
   $('#myStopWatch').on('mouseout', function(e) {
-      timerElement = null;
+    timerElement = null;
   });
+
+  $('.icon').on('mouseover', function(e) {
+    iconElement = $(e.currentTarget);
+  });
+
+  $('.icon').on('mouseout', function(e) {
+    iconElement = null;
+  });
+
+// Listening for keypresses
+// 61 is (+=) key
+// 45 is (-_) key
+// Sizes are changed by width (for icons) or font-size (for timer)
+// 10 px is the amount of change atm
 
   $(document).keypress(function(e) {
-      if (e.which == 61 && timerElement) {
-        var newPxSize = parseInt($(".time").css("font-size"));
-        if (30 <= newPxSize && newPxSize < 100) {
-          newPxSize += 10;
-          console.log(newPxSize);
-          newPxSize = newPxSize + "" + "px";
-          console.log(newPxSize);
-          $(".time").css("font-size", newPxSize);
-        };
-      }
+    if (e.which == 61 && timerElement) {
+      var newPxSize = parseInt($(".time").css("font-size"));
+      if (30 <= newPxSize && newPxSize < 100) {
+        newPxSize += 10;
+        newPxSize = newPxSize + "" + "px";
+        $(".time").css("font-size", newPxSize);
+      };
+    }
 
-      if (e.which == 45 && timerElement) {
-        var newPxSize = parseInt($(".time").css("font-size"));
-        if (30 < newPxSize && newPxSize <= 100) {
-          newPxSize -= 10;
-          console.log(newPxSize);
-          newPxSize = newPxSize + "" + "px";
-          console.log(newPxSize);
-          $(".time").css("font-size", newPxSize);
-        };
-      }
+    if (e.which == 45 && timerElement) {
+      var newPxSize = parseInt($(".time").css("font-size"));
+      if (30 < newPxSize && newPxSize <= 100) {
+        newPxSize -= 10;
+        newPxSize = newPxSize + "" + "px";
+        $(".time").css("font-size", newPxSize);
+      };
+    }
+
+    if (e.which == 61 && iconElement) {
+      var newPxSize = parseInt($("#" + iconElement[0].id).css("width"));
+      if (30 <= newPxSize && newPxSize < 100) {
+        newPxSize += 10;
+        newPxSize = newPxSize + "" + "px";
+        $("#" + iconElement[0].id).css("width", newPxSize);
+      };
+    }
+
+    if (e.which == 45 && iconElement) {
+      var newPxSize = parseInt($("#" + iconElement[0].id).css("width"));
+      if (30 < newPxSize && newPxSize <= 100) {
+        newPxSize -= 10;
+        newPxSize = newPxSize + "" + "px";
+        $("#" + iconElement[0].id).css("width", newPxSize);
+      };
+    }
   });
+
+// To added and subtract amount of circles in play
+// logs variable for amount of circles shown
 
   $("#addCircle").on("click", function () {
     if (circleCount < 9) {
@@ -47,6 +81,8 @@ $(document).ready(function () {
       circleCount--;
     };
   });
+
+// gives the draggable functionality
 
   $(".draggable").draggable();
 })
