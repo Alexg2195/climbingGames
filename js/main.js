39751ae27ce4c19d64f1704ac1d35d163f5435ba
Alexg2195 <alexg2195@gmail.com>
2016-02-07 14:33:25 -0500

    consolidated code and have working stop to game when player finishes

--- js/main.js
+++ js/main.js
@@ -1,68 +1,37 @@
 $(document).ready(function () {
-  var circleCount = 0;
+  var circleCount = 1;
   var touchCount = 0;
   var timerElement = null;
   var iconElement = null;
   var gameRunning = false;
 
-// To added and subtract amount of circles in play
-// logs variable for amount of circles shown
-
-  $("#addCircle").on("click", function () {
-    if (circleCount < 9 && gameRunning === false) {
-      circleCount++;
-      $("#icon" + circleCount).css("display", "inline-block");
-    };
-  });
-
-  $("#minusCircle").on("click", function () {
-    if (circleCount > 0 && gameRunning === false) {
-      $("#icon" + circleCount).css("display", "none");
-      circleCount--;
-    };
-  });
-
-// Logging mouse over data for the timer and icons as a variable
-
-  $('#myStopWatch').on('mouseover', function(e) {
-    timerElement = $(e.currentTarget);
-  });
-
-  $('#myStopWatch').on('mouseout', function(e) {
-    timerElement = null;
-  });
-
-  $('.icon').on('mouseover', function(e) {
-    iconElement = $(e.currentTarget);
-  });
-
-  $('.icon').on('mouseout', function(e) {
-    iconElement = null;
-  });
-
 // Listening for keypresses
 // 61 is (+=) key
 // 45 is (-_) key
 // 32 is spacebar
+// 13 = ENTER
+// 92 = \ key
 
   $(document).keypress(function(e) {
     // console.log(e.which);
 
-// 13 = ENTER. Sets gameRunning variable to allow controls or not
+// Sets gameRunning variable to allow controls or not
 
     if (e.which === 13) {
       if (gameRunning === false) {
         gameRunning = true;
+        unbindDrag();
       } else {
         gameRunning = false;
+        bindDrag();
       }
     };
 
-// 92 = \ key. Will turn gameRunning variable off to stop game controls
+// Will turn gameRunning variable off to stop game controls
 
     if (e.which === 92) {
       gameRunning = false;
-      
+      bindDrag();
     };
 
 // Space bar takes away the circles as the player climbs
@@ -70,6 +39,9 @@ $(document).ready(function () {
     if (e.which === 32 && gameRunning === true) {
       touchCount++;
       $("#icon" + touchCount).addClass('zoomOut');
+      if (touchCount === circleCount) {
+        startStop();
+      };
     };
 
 // Sizes are changed by width (for icons) or font-size (for timer)
@@ -112,7 +84,130 @@ $(document).ready(function () {
     }
   });
 
-// gives the draggable functionality
+// To added and subtract amount of circles in play
+// logs variable for amount of circles shown
 
-  $(".draggable").draggable();
+  $("#addCircle").on("click", function () {
+    if (circleCount < 9 && gameRunning === false) {
+      circleCount++;
+      $("#icon" + circleCount).css("display", "inline-block");
+    };
+  });
+
+  $("#minusCircle").on("click", function () {
+    if (circleCount > 1 && gameRunning === false) {
+      $("#icon" + circleCount).css("display", "none");
+      circleCount--;
+    };
+  });
+
+// Logging mouse over data for the timer and icons as a variable
+
+  $('#myStopWatch').on('mouseover', function(e) {
+    timerElement = $(e.currentTarget);
+  });
+
+  $('#myStopWatch').on('mouseout', function(e) {
+    timerElement = null;
+  });
+
+  $('.icon').on('mouseover', function(e) {
+    iconElement = $(e.currentTarget);
+  });
+
+  $('.icon').on('mouseout', function(e) {
+    iconElement = null;
+  });
+
+// gives the draggable functionality
+  
+  var bindDrag = function () {
+    $(".draggable").draggable({ disabled: false });
+  }
+
+  var unbindDrag = function () {
+    $(".draggable").draggable({ disabled: true });
+  }
+
+  bindDrag();
+
+// Timer
+
+  var miliSec = 0;
+  var sec = 0;
+  var min = 0;
+  var timer = 0;
+  var timing = false;
+
+  $(document).keypress(function (event) {
+    // console.log(event.which);
+    switch (event.which) {
+      case 13:
+        startStop();
+        break;
+      case 92:
+        reset();
+        break;
+      default:
+        break;
+    }
+  });
+
+  function startStop () {
+    if (!timing) {
+      startTimer();
+    } else {
+      stopTimer();
+    };
+  };
+
+  function stopTimer () {
+    timing = false;
+    clearInterval(timer);
+  };
+
+  function startTimer () {
+    if (!timing) {
+      timing = true;
+      timer = setInterval(runStopWatch, 100);
+    };
+  };
+
+  function runStopWatch () {
+    miliSec += 10;
+
+    if (miliSec > 99) {
+      miliSec = 0;
+      sec ++;
+    };
+
+    if (sec > 59) {
+      sec = 0;
+      min ++;
+    };
+
+    printTime();
+  };
+
+  function reset () {
+    if (timing) {
+      stopTimer();
+    };
+    miliSec = 0;
+    sec = 0;
+    min = 0;
+    printTime();
+  };
+
+  function printTime (argument) {
+    $(".min").text(pad(min));
+    $(".sec").text(pad(sec));
+    $(".miliSec").text(pad(miliSec));
+  };
+
+  function pad(num) {
+    var newNum = num.toString();
+    while (newNum.length < 2) newNum = "0" + newNum;
+    return newNum;
+  };
 });
